a7edd1e08bf8d1f34c25c1dcf84489cf5a282066
minsung.Joh <jms5049@naver.com>
2018-04-20 22:42:38 +0900

    WK01 - k means clustering

--- K means Clustering and Hierarachical Clusting.R
+++ K means Clustering and Hierarachical Clusting.R
@@ -15,3 +15,13 @@ plot(x1,x2)
 
 #result matrix
 z <- cbind(x1,x2)
+
+#k means clustering
+kc <- kmeans(z,2)
+
+str(kc)
+kc$centers
+kc$cluster
+
+plot(z, col = kc$cluster, xlab = "x1" , ylab ="x2")
+points(kc$centers, col = 1:3, pch = 8, cex=2)
