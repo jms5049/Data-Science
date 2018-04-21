34393fd20aead9f300178638a2893de2c1373ff4
minsung.Joh <jms5049@naver.com>
2018-04-21 20:01:44 +0900

    WK02 set dummy dataframe in order to pre-process

--- Linear Regression.R
+++ Linear Regression.R
@@ -9,3 +9,16 @@ rawdata <- subset(rawdata, select = c(Price,Age_08_04,KM,Fuel_Type,HP,Color))
 
 #catergorize cars with their Fuel Types
 unique_type <- unique(rawdata$Fuel_Type)
+
+#create dummy dataframe
+type_dummy = as.data.frame(matrix(0,nrow(rawdata), length(unique_type)))
+
+#read toyota dataset's Fuel Type and write into dummy dataframe
+for(i in 1:(length(unique_type))){
+
+	tmp = unique_type[i]
+	tmp_idx = which(rawdata$Fuel_Type == tmp)
+	type_dummy[tmp_idx,i] = 1
+	colnames(type_dummy)[i] = sprintf("type_%s",tmp)
+
+}
