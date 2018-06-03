a8bc3a7e6bce4cc0d2d6ed8e734ff1415cf4a821
minsung Joh <jms5049@naver.com>
2018-06-03 14:14:58 +0900

    WK02 - Data Preprocessed

--- Classification with k-NN,DT,LR,NN,SVM.R
+++ Classification with k-NN,DT,LR,NN,SVM.R
@@ -31,3 +31,12 @@ for(i in 1:ncol(rawdata)){
 	rawdata[,i] = (rawdata[,i] - min(rawdata[,i])) / (max(rawdata[,i]) - min(rawdata[,i]))
 }
 
+## combine scaled raw data and number changed target
+prdata = cbind(rawdata, new_gender)
+
+tmp_idx = union(which(prdata$new_gender == 1), which(prdata$new_gender == 2))
+prdata = prdata[tmp_idx,]
+prdata$new_gender[which(prdata$new_gender == 2)] = 0
+prdata$new_gender = as.factor(prdata$new_gender)
+
+
