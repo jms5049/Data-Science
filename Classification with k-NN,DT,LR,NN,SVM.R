333dea47602ab8fa4b5a16b1a2028961bbdec13e
minsung Joh <jms5049@naver.com>
2018-06-04 14:17:26 +0900

    WK03 - DT

--- Classification with k-NN,DT,LR,NN,SVM.R
+++ Classification with k-NN,DT,LR,NN,SVM.R
@@ -55,3 +55,70 @@ out_knn = knn(trn_data[,1:(ncol(trn_data)-1)], tst_data[,1:(ncol(tst_data)-1)], 
 model_lr = glm(new_gender ~., data = trn_data, family = "binomial")
 out_lr = predict(model_lr, tst_data)
 
+## Decision Tree
+library(rpart)
+model_tree = rpart(new_gender ~., data = trn_data, control = rpart.control(minsplit = 20))
+out_tree = predict(model_tree, tst_data)
+
+## Tree Visualization
+plot(model_tree, margin=0.2)
+text(model_tree, use.n = TRUE)
+
+## Neural Networks
+library(nnet)
+
+model_nn = nnet(new_gender ~., data = trn_data, size=80, linout=FALSE, maxit=300)
+out_nn = predict(model_nn, tst_data, type= "class")
+
+## NN visualization
+library(devtools)
+source_url('https://gist.githubusercontent.com/fawda123/7471137/raw/466c1474d0a505ff044412703516c34f1a4684a5/nnet_plot_update.r')
+library(reshape2)
+plot.nnet(model_nn)
+
+## SVM
+library(e1071)
+model_svm = svm(new_gender ~., data = trn_data, type="C-classification", kernel="linear", gamma=10, cost=100)
+out_svm = predict(model_svm, tst_data)
+
+## Analuze Result
+
+target = tst_data[,ncol(tst_data)]
+outs = cbind(target, out_knn)
+
+tmp_idx1 = which(out_lr >= 0)
+tmp_idx2 = which(out_lr < 0)
+out_lr2 = out_lr
+out_lr2[tmp_idx1] = 1
+out_lr2[tmp_idx2] = 0
+outs = cbind(outs, out_lr2)
+
+out_tree2 = out_tree[,2]
+tmp_idx1 = which(out_tree2 >= 0.5)
+tmp_idx2 = which(out_tree2 < 0.5)
+out_tree2[tmp_idx1] = 1
+out_tree2[tmp_idx2] = 0
+outs = cbind(outs, out_tree2)
+
+outs = cbind(outs, out_nn, out_svm)
+
+for( i in 1:ncol(outs)){
+	if(length(which(outs[,i]==2)) == 0){
+		next
+	}
+	tmp_idx1 = which(outs[,i]==2)
+	tmp_idx2 = which(outs[,i]==1)
+	
+	outs[tmp_idx1,i] = 1
+	outs[tmp_idx2,i] = 0
+}
+
+##print analysis
+library(caret)
+library(lattice)
+confusionMatrix(factor(outs[,2]), factor(outs[,1]))
+confusionMatrix(factor(outs[,3]), factor(outs[,1]))
+confusionMatrix(factor(outs[,4]), factor(outs[,1]))
+confusionMatrix(factor(outs[,5]), factor(outs[,1]))
+confusionMatrix(factor(outs[,6]), factor(outs[,1]))
+
